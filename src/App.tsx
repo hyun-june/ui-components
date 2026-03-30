4308bae4e788d00b04150ed9a0dd9c14c8772fe7
hyunjune <leehyunjune11@naver.com>
2026-03-31 01:51:32 +0900

    test 브랜치 분기

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="flex h-screen items-center justify-center">
       <SuccessAnimation />
       {/* <ProjectCard /> */}
-      <div>test/main</div>
+      <div>test/main-branch</div>
     </div>
   );
 }
